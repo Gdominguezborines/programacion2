d63b92133647dc28562647c54fe83bdf7b76779a
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2020-10-27 11:23:34 +0100

    1 comit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    System.out.println("hola chicos");
+    System.out.println("hola chicos que tal estais");
 
     }
 }
